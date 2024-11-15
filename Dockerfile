0fc62ffa7354fd3f8b56c265c65112dacd1268bd
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-11-15 10:57:55 +0100

    feat: organize experimental results to avoid repetitions

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ ARG PYTHON_VERSION=3.12
 FROM python:${PYTHON_VERSION}
 RUN mkdir -p /experiment
 VOLUME "/data"
-ENV OUTPUT_DIR=/data
+ENV DATA_DIR=/data
 WORKDIR /experiment
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY * /experiment/
 ENV OWNER=1000:1000
-CMD python experiment.py | tee ${OUTPUT_DIR}/output.log && \
-    chown -R ${OWNER} ${OUTPUT_DIR}
+CMD export OUTPUT_DIR=$DATA_DIR/$(date +%Y-%m-%d-%H-%M-%S) && \
+    mkdir -p $OUTPUT_DIR && \
+    python experiment.py | tee $OUTPUT_DIR/output.log && \
+    chown -R $OWNER $DATA_DIR
--- experiment.py
+++ experiment.py
@@ -9,20 +9,39 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 from pathlib import Path
+from hashlib import sha512
+import json
+import sys
 
 
+# Set up environment hyper-parameters from the environment, with safe defaults
 DATASET_ID = int(os.getenv('DATASET_ID', "61"))
 TEST_SIZE = float(os.getenv('TEST_SIZE', "0.2"))
 RANDOM_STATE = int(os.getenv('RANDOM_STATE', "42"))
 K_FOLDS = int(os.getenv('K_FOLDS', "5"))
 SCORING = os.getenv('SCORING', 'accuracy')
 PICTURE_SIZE = tuple(map(int, os.getenv('PICTURE_SIZE', '10,6').split(',')))
-OUTPUT_DIR = Path(os.getenv('OUTPUT_DIR', '.'))
+
+# Compute a unique experiment ID based on the hyper-parameters
+FOOTPRINT_KEYS = {'DATASET_ID', 'TEST_SIZE', 'RANDOM_STATE', 'K_FOLDS', 'SCORING', 'PICTURE_SIZE'}
+EXPERIMENT_FOOTPRINT = {k: v for k, v in locals().items() if k in FOOTPRINT_KEYS}
+EXPERIMENT_ID = sha512(json.dumps(EXPERIMENT_FOOTPRINT, sort_keys=True).encode()).hexdigest()
+
+# Ensure the output directory exists
+DATA_DIR = Path(os.getenv('DATA_DIR', './data'))
+OUTPUT_DIR = Path(os.getenv('OUTPUT_DIR', str(DATA_DIR)))
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+# Do not waste time if the experiment has already been run
+# by looking for the existence of a .json file named after the EXPERIMENT_ID
+for file in DATA_DIR.glob('**/*.json'):
+    if file.is_file() and file.stem == EXPERIMENT_ID:
+        print(f'Experiment with ID {EXPERIMENT_ID[:8]} already exists in {file.parent}', file=sys.stderr)
+        exit(0)
 
 
 # Load the Poker Hand dataset from OpenML
-dataset = openml.datasets.get_dataset(DATASET_ID, download_all_files=True)  # iris
+dataset = openml.datasets.get_dataset(DATASET_ID, download_all_files=True)
 X, y, _, _ = dataset.get_data(target=dataset.default_target_attribute)
 
 # Ensure test set contains at least one instance of each class
@@ -54,6 +73,7 @@ plt.boxplot(results.values(), labels=results.keys())
 plt.title('Validation Set Performance of Models')
 plt.ylabel('Accuracy')
 plt.xlabel('Model')
+# Save the plot to OUTPUT_DIR/graph.png
 plt.savefig(OUTPUT_DIR / 'graph.png')
 
 # Select the best model and evaluate on test set
@@ -68,6 +88,11 @@ test_accuracy = accuracy_score(y_test, y_pred)
 print(f'Best Model: {best_model_name}')
 print(f'Test Set Accuracy: {test_accuracy:.4f}')
 
-
+# Save the best model to OUTPUT_DIR/model.pkl
 with open(OUTPUT_DIR / 'model.pkl', 'wb') as file:
     pickle.dump(best_model, file)
+
+
+# Save experiment footprint into OUTPUT_DIR/EXPERIMENT_ID.json
+with open(OUTPUT_DIR / f'{EXPERIMENT_ID}.json', 'w') as file:
+    json.dump(EXPERIMENT_FOOTPRINT, file, indent=4)
