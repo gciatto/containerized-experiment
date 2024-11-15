829353426f4044b1f72a5dc03027580580ad6246
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-11-15 13:01:26 +0100

    feat: support gridcv models parametrization

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY * /experiment/
 ENV OWNER=1000:1000
-CMD export OUTPUT_DIR=$DATA_DIR/$(date +%Y-%m-%d-%H-%M-%S) && \
+CMD export OUTPUT_DIR=$DATA_DIR/$(date +%Y-%m-%d-%H-%M-%S)-$(hostname) && \
     mkdir -p $OUTPUT_DIR && \
     python experiment.py | tee $OUTPUT_DIR/output.log && \
     chown -R $OWNER $DATA_DIR
--- experiment.py
+++ experiment.py
@@ -1,17 +1,43 @@
 import openml
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import pickle
 import os
 from pathlib import Path
 from hashlib import sha512
-import json
+import yaml
 import sys
+
+
+def get_grid_search_entry_from_env(env_var_name):
+    if env_var_name.startswith('MODEL_'):
+        name = env_var_name[len('MODEL_'):]
+    else:
+        name = env_var_name
+        env_var_name = f'MODEL_{name}'
+    hyperparams = os.environ[env_var_name]
+    hyperparams = yaml.safe_load(hyperparams)
+    assert len(hyperparams) == 1, f"Too many keys in {env_var_name}"
+    for k, v in hyperparams.items():
+        return {name: [k, v]}
+
+
+def get_grid_search_entries_from_env():
+    entries = {}
+    for k in os.environ:
+        if k.startswith('MODEL_'):
+            for name, params in get_grid_search_entry_from_env(k).items():
+                entries[name] = params
+    return entries
+
+
+def create_scikit_learn_model(class_name):
+    import importlib
+    module_name, class_name = class_name.rsplit('.', 1)
+    module = importlib.import_module(module_name)
+    klass = getattr(module, class_name)
+    return klass()
 
 
 # Set up environment hyper-parameters from the environment, with safe defaults
@@ -21,11 +47,13 @@ RANDOM_STATE = int(os.getenv('RANDOM_STATE', "42"))
 K_FOLDS = int(os.getenv('K_FOLDS', "5"))
 SCORING = os.getenv('SCORING', 'accuracy')
 PICTURE_SIZE = tuple(map(int, os.getenv('PICTURE_SIZE', '10,6').split(',')))
+MODELS = get_grid_search_entries_from_env()
 
 # Compute a unique experiment ID based on the hyper-parameters
-FOOTPRINT_KEYS = {'DATASET_ID', 'TEST_SIZE', 'RANDOM_STATE', 'K_FOLDS', 'SCORING', 'PICTURE_SIZE'}
+FOOTPRINT_KEYS = {'DATASET_ID', 'TEST_SIZE', 'RANDOM_STATE', 'K_FOLDS', 'SCORING', 'PICTURE_SIZE', 'MODELS'}
 EXPERIMENT_FOOTPRINT = {k: v for k, v in locals().items() if k in FOOTPRINT_KEYS}
-EXPERIMENT_ID = sha512(json.dumps(EXPERIMENT_FOOTPRINT, sort_keys=True).encode()).hexdigest()
+EXPERIMENT_FOOTPRINT_YAML = yaml.dump(EXPERIMENT_FOOTPRINT, sort_keys=True)
+EXPERIMENT_ID = sha512(EXPERIMENT_FOOTPRINT_YAML.encode()).hexdigest()
 
 # Ensure the output directory exists
 DATA_DIR = Path(os.getenv('DATA_DIR', './data'))
@@ -33,11 +61,16 @@ OUTPUT_DIR = Path(os.getenv('OUTPUT_DIR', str(DATA_DIR)))
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 # Do not waste time if the experiment has already been run
-# by looking for the existence of a .json file named after the EXPERIMENT_ID
-for file in DATA_DIR.glob('**/*.json'):
+# by looking for the existence of a .yaml file named after the EXPERIMENT_ID
+for file in DATA_DIR.glob('**/*.yaml'):
     if file.is_file() and file.stem == EXPERIMENT_ID:
         print(f'Experiment with ID {EXPERIMENT_ID[:8]} already exists in {file.parent}', file=sys.stderr)
         exit(0)
+
+
+print(f'Running experiment ID {EXPERIMENT_ID[:8]}', file=sys.stderr)
+print(f'Output directory: {OUTPUT_DIR}', file=sys.stderr)
+print(f'Experiment footprint:\n\t{EXPERIMENT_FOOTPRINT_YAML.replace("\n", "\n\t")}', file=sys.stderr)
 
 
 # Load the Poker Hand dataset from OpenML
@@ -48,12 +81,8 @@ X, y, _, _ = dataset.get_data(target=dataset.default_target_attribute)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, stratify=y, random_state=RANDOM_STATE)
 
 # Define classifiers and hyperparameters for GridSearchCV
-classifiers = {
-    'DecisionTree': (DecisionTreeClassifier(), {'max_depth': [None, 10, 20], 'min_samples_split': [2, 10, 20]}),
-    'MLP': (MLPClassifier(), {'hidden_layer_sizes': [(50,), (100,), (50, 50)], 'activation': ['tanh', 'relu'], 'solver': ['adam', 'sgd']}),
-    'LogisticRegression': (LogisticRegression(), {'C': [0.1, 1, 10], 'solver': ['liblinear', 'lbfgs']}),
-    'SVM': (SVC(), {'C': [0.1, 1, 10], 'kernel': ['linear', 'rbf']}),
-}
+classifiers = {name: (create_scikit_learn_model(params[0]),) + tuple(params[1:]) for name, params in MODELS.items()}
+print(f'Start GridSearchCV for the following models/parameters: {classifiers}', file=sys.stderr)
 
 # Run GridSearchCV for each classifier and store results
 results = {}
@@ -93,6 +122,6 @@ with open(OUTPUT_DIR / 'model.pkl', 'wb') as file:
     pickle.dump(best_model, file)
 
 
-# Save experiment footprint into OUTPUT_DIR/EXPERIMENT_ID.json
-with open(OUTPUT_DIR / f'{EXPERIMENT_ID}.json', 'w') as file:
-    json.dump(EXPERIMENT_FOOTPRINT, file, indent=4)
+# Save experiment footprint into OUTPUT_DIR/EXPERIMENT_ID.yaml
+with open(OUTPUT_DIR / f'{EXPERIMENT_ID}.yaml', 'w') as file:
+    yaml.dump(EXPERIMENT_FOOTPRINT, file)
